75846646f4d45430b31b9a87ddd8248757ad4fd8
David <callejasdavid16@gmail.com>
2022-02-15 18:04:34 +0400

    due to API limits of 500 requests per second, was added a sleep function of 1 second in order to be able to get data from a mask 16 network segment

--- project.py
+++ project.py
@@ -1,24 +1,28 @@
 from shodan import Shodan
-import json, ipaddress
+import json, ipaddress, time
 
 ### Reference - https://developer.shodan.io/api/clients
 api = Shodan('<API-Key>')
-IP = [str(ip) for ip in ipaddress.IPv4Network('<Public IP segment>')] ### example 85.269.45.0/24
 PSEARCH = 3389 ## Port to search as a int
 
 def run():
   print("IP Address   | Open Port")
-  for item in IP:
-    try:
-      ipinfo = json.dumps(api.host(item))
-      data = json.loads(ipinfo)['data']
-      ipAddr = [ipinfo['ip_str'] for ipinfo in data]
-      ports = [ipinfo['port'] for ipinfo in data]
-      for ip, port in zip(ipAddr, ports):
-        if port == PSEARCH:
-          print(ip, "   | ", port)
-    except:
-      pass
+  for x in range(1, 255):
+    value = str(x)
+    ipSeg = '85.269.'+value+'.0/24' ### example 85.269.45.0/24
+    ipMask = [str(ip) for ip in ipaddress.IPv4Network(ipSeg)]
+    time.sleep(1)
+    for item in ipMask:
+      try:
+        ipinfo = json.dumps(api.host(item))
+        data = json.loads(ipinfo)['data']
+        ipAddr = [ipinfo['ip_str'] for ipinfo in data]
+        ports = [ipinfo['port'] for ipinfo in data]
+        for ip, port in zip(ipAddr, ports):
+          if port == PSEARCH:
+            print(ip, "   | ", port)
+      except:
+        pass
 
 if __name__ == '__main__':
   run()
